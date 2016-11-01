6afeec7cefabe036945a0985250008e66c9db72d
sambhav13 <sambhavgupta4@gmail.com>
2016-11-01 20:44:04 +0530

    code file added

---
+++ build.sbt
@@ -0,0 +1,7 @@
+name := "SparkReadProj"
+
+version := "1.0"
+
+scalaVersion := "2.11.7"
+
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.0.0"
---
+++ src/main/scala/Test.scala
@@ -0,0 +1,18 @@
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+object Test{
+
+	def main(args :Array[String]): Unit = {
+
+
+		println("hello sam")
+		val conf = new SparkConf().setAppName("Spark 2.0.0 App").setMaster("local[2]")
+		val sc = new SparkContext(conf)
+		val data = Array(1, 2, 3, 4, 5)
+		val distData = sc.parallelize(data)
+	
+		distData.collect().foreach(x => println(x))
+		distData.saveAsTextFile("NumberRDD");
+	}
+}
